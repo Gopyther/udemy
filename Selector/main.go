a4f8f7632f2bcd3170e3b6c48a6bc85f4c9c5e03
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-02-11 10:28:10 +0100

    error setting

--- Selector/main.go
+++ Selector/main.go
@@ -1,44 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
+var ErrCrewNotFound = errors.New("Crew member not found")
 var scMapping = map[string]int{
-	"james": 5,
+	"James": 5,
 	"Kevin": 10,
 	"Rahul": 9,
 }
 
-func findSC(name, server string, c chan int) {
-	//Simulate searching
-	time.Sleep(time.Duration(rand.Intn(50)) * time.Minute)
+type findError struct {
+	Name, Server, Msg string
+}
 
-	//return security clearance from map
-	c <- scMapping[name]
+func (e findError) Error() string {
+	return e.Msg
+}
+
+func findSC(name, server string) (int, error) {
+	//Simulate searching
+	time.Sleep(time.Duration(rand.Intn(50)) * time.Microsecond)
+
+	if v, ok := scMapping[name]; !ok {
+		return -1, findError{name, server, "Crew member not found"}
+	} else {
+		return v, nil
+	}
 }
 
 func main() {
-
 	rand.Seed(time.Now().UnixNano())
-
-	c1 := make(chan int)
-	c2 := make(chan int)
-
-	name := "James"
-
-	go findSC(name, "Server 1", c1)
-	go findSC(name, "Server 2", c2)
-
-	select {
-	case sc := <-c1:
-		fmt.Println(name, "has a security clearance of ", sc, "found in server1 ")
-	case sc := <-c2:
-		fmt.Println(name, "has a security clearance of ", sc, "found in server2 ")
-	case <-time.After(1 * time.Millisecond):
-		fmt.Println("Search timed out!!")
+	if clearance, err := findSC("Ruko", "Server 1"); err != nil {
+		fmt.Println("Error occured while searching ", err)
+		if v, ok := err.(findError); ok {
+			fmt.Println("Server name", v.Server)
+			fmt.Println("Crew member name", v.Name)
+		}
+	} else {
+		fmt.Println("Crew member has security clearance:", clearance)
 	}
-
 }
+
+// func main() {
+
+// 	rand.Seed(time.Now().UnixNano())
+
+// 	c1 := make(chan int)
+// 	c2 := make(chan int)
+
+// 	name := "James"
+
+// 	go findSC(name, "Server 1", c1)
+// 	go findSC(name, "Server 2", c2)
+
+// 	/*
+// 		Do some work...
+// 	*/
+
+// 	select {
+// 	case sc := <-c1:
+// 		fmt.Println(name, "has a security clearance of ", sc, "found in server1 ")
+// 	case sc := <-c2:
+// 		fmt.Println(name, "has a security clearance of ", sc, "found in server2 ")
+// 	default:
+// 		fmt.Println("Too slow!!")
+// 	}
+
+// }
