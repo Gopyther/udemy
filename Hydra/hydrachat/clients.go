69b7c1c871e37f8d59a962c877a28d6ed72be244
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-03-15 18:41:01 +0100

    hydra chat

---
+++ Hydra/hydrachat/clients.go
@@ -0,0 +1,53 @@
+package hydrachat
+
+import (
+	"bufio"
+	"io"
+)
+
+type client struct {
+	*bufio.Reader
+	*bufio.Writer
+	wc chan string
+}
+
+func StartClient(msgCH chan<- string, cn io.ReadWriteCloser, quit chan struct{}) (chan<- string, <-chan struct{}) {
+	c := new(client)
+	c.Reader = bufio.NewReader(cn)
+	c.Writer = bufio.NewWriter(cn)
+	c.wc = make(chan string)
+	done := make(chan struct{})
+
+	//setup the reader
+	go func() {
+		scanner := bufio.NewScanner(c.Reader)
+		for scanner.Scan() {
+			logger.Println(scanner.Text())
+			msgCH <- scanner.Text()
+		}
+		done <- struct{}{}
+	}()
+
+	// setup the writer
+	c.writerMonitor()
+
+	go func() {
+		select {
+		case <-quit:
+			cn.Close()
+		case <-done:
+		}
+	}()
+	return c.wc, done
+
+}
+
+func (c *client) writerMonitor() {
+	go func() {
+		for s := range c.wc {
+			//logger.Println("Sending", s)
+			c.WriteString(s + "\n")
+			c.Flush
+		}
+	}()
+}
